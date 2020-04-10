c717c1b398e1ba25f5b0b3200a0e58d8dbc1fbaf
tako_eng <45517531+tako-eng@users.noreply.github.com>
2020-04-10 16:42:54 +0900

    コメント追加

--- curl_statuscode_body.sh
+++ curl_statuscode_body.sh
@@ -1,12 +1,22 @@
 #!/bin/bash -
 
+# curl先URL
 url='curl -X GET http://example.com/ -o - -w '%{http_code}' -sS' 
+
+# curl実行
 urlexec=`${url}`
+
+# httpステータスコード
+# -w '%{http_code}'オプションにより最終行に取得するため切り出す
 httpstatus=`echo "$urlexec" | tail -n 1`
+
+# レスポンスボディ
 body=`echo "$urlexec" | sed '$d'`
 
-echo $urlexec
-echo "*******************"
-echo $httpstatus
-echo "*******************"
-echo $body
+###############
+# echo $httpstatus
+# 200
+
+# echo $body
+# <!doctype html> <html> <head> <title>Example Domain< ...
+###############
